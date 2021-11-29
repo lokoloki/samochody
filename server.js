b1681e3f27541e752da8dcad45ffc1cbcc9f584f
lokoloki <95242794+lokoloki@users.noreply.github.com>
2021-11-29 18:56:30 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var hbs = require("express-handlebars");
 var express = require("express");
 var app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 var path = require("path");
 
